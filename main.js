a8dfeadf7dc20184c1877fab36e704139440f4e3
= <=>
2020-03-15 21:11:05 -0700

    Added rotations, reset, and added the bird

--- main.js
+++ main.js
@@ -1,50 +1,62 @@
 /* Author: Sean Langley */
 /* Date: March 7 2020   */
 
-var flag = true;
 var canvas = document.getElementById("myCanvas");
 var score_text = document.getElementById("score");
+var high_score_text = document.getElementById("High score");
+var lose_text = document.getElementById("lose text");
+var restart_button = document.getElementById("restart button");
+var flappy = document.getElementById("flappy")
+var high_score = 0;
 var ctx = canvas.getContext("2d");
+var hitbox = false;
 var pipes = [];
-var player = (function(){
-    var height = 50;
-    return {
-        width: height,
-        height: height,
-        x: 20,
-        y: canvas.height-height,
-        vel: 0,
-        score: 0
-    }
-})();
-
-newPipePair();
+var player;
+var updateloop;
 
 document.addEventListener('keydown', function(event) {
     if (event.key == " ") {
         player.vel = 10;
+        player.angular_momentum = 20;
+        player.has_jumped = true;
     }
 });
 
-var updateloop = setInterval(update, 20);
+restart();
 
 function update(){
-    fill(player, "white");
+    update_player("disable");
     player.y -= player.vel;
     player.vel -= 1;
+
     if (player.y > canvas.height - player.height){
         player.vel = 0;
         player.y = canvas.height - player.height;
     }
-    fill(player, "red");
+    if (player.has_jumped){
+        if (degrees(player.angle) < degrees(-30)){
+            player.angular_momentum = -1;
+        }
+        player.angle -= player.angular_momentum;
+        player.angular_momentum -= 1;
+        if (degrees(player.angle) > degrees(30)){
+            player.angular_momentum = 0;
+        }
+    }
+    update_player("enable");
 
     for (i = 0; i < pipes.length; i++){
-        fill(pipes[i], "white");
+        clear(pipes[i]);
         pipes[i].x -= 10;
-        fill(pipes[i], "red");
+        fill(pipes[i]);
         if (didCollide(player, pipes[i])) {
             clearInterval(updateloop);
-            score_text.insertAdjacentHTML("afterend", "<p>You lose</p>");
+            if (player.score > high_score){
+                high_score = player.score;
+                high_score_text.innerHTML = "High score: " + high_score;
+            }
+            lose_text.innerHTML = "You lose!";
+            break;
         }
     }
 
@@ -52,16 +64,11 @@ function update(){
         pipes.shift();
         pipes.shift();
     }
-    last_pipe = pipes[pipes.length -1];
 
+    last_pipe = pipes[pipes.length -1];
     if (last_pipe.x + last_pipe.width < canvas.width - 200){
         newPipePair();
     }
-}
-
-function fill(obj, color){
-    ctx.fillStyle = color;
-    ctx.fillRect(obj.x, obj.y, obj.width, obj.height);
 }
 
 function didCollide(player, pipe){
@@ -105,6 +112,78 @@ function newPipe(orientation, height){
     }
 }
 
+function restart(){
+    restart_button.blur();
+    lose_text.innerHTML = "";
+    score_text.innerHTML = "Score: 0";
+    high_score_text.innerHTML = "High score: " + high_score;
+    for(var i = 0; i < pipes.length; i++){
+        clear(pipes[i]);
+    }
+    pipes.length = 0;
+    newPipePair();
+    if (typeof player != "undefined"){
+        update_player("disable");
+    }
+    player = (function(){
+        var height = 50;
+        return {
+            width: height,
+            height: height,
+            angular_momentum: 0,
+            angle: 0,
+            x: 20,
+            y: canvas.height-height,
+            vel: 0,
+            score: 0,
+            has_jumped: false,
+        }
+    })();
+    update_player("enable");
+    clearInterval(updateloop);
+    updateloop = setInterval(update, 20);
+}
+
+function update_player(new_state){
+    ctx.translate(player.x, player.y);
+    ctx.rotate(degrees(player.angle));
+    ctx.translate(-player.x, -player.y);
+    if (hitbox && new_state == "enable"){
+        ctx.fillStyle = "red";
+    }
+    else if(new_state == "disable"){
+        ctx.fillStyle = "White";
+    }
+    ctx.fillRect(player.x, player.y, player.width, player.height);
+    if (new_state == "enable"){
+        var scale = 1.75;
+        var flappy_side = player.width * scale;
+        ctx.drawImage(flappy, player.x-15, player.y-15, 
+            player.width*scale, player.height*scale);
+    }
+    ctx.translate(player.x, player.y);
+    ctx.rotate(degrees(-player.angle));
+    ctx.translate(-player.x, -player.y);
+}
+
+function fill(obj){
+    if (typeof obj !== "undefined"){
+        ctx.fillStyle = "red";
+        ctx.fillRect(obj.x, obj.y, obj.width, obj.height);
+    }
+}
+
+function clear(obj){
+    if (typeof obj !== "undefined"){
+        ctx.fillStyle = "white";
+        ctx.fillRect(obj.x, obj.y, obj.width, obj.height);
+    }
+}
+
 function RandomInRange(min, max){
     return Math.floor(Math.random() * (max - min) ) + min;
+}
+
+function degrees(angle){
+    return angle*Math.PI/180;
 }
